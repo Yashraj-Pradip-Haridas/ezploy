d7275d332297cf382d57b6e595bae6d3e25114b9
yashraj-pradip-haridas <yashraj.haridas22@pccoepune.org>
2026-06-27 19:52:20 +0530

    project init

---
+++ src/main/java/org/haridas/ezploy/EzployApplication.java
@@ -0,0 +1,13 @@
+package org.haridas.ezploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/haridas/ezploy/EzployApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haridas.ezploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
