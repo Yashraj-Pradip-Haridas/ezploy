ac0d47b03b940a9cf04f80a0f399650d12bf6bb0
yashraj-pradip-haridas <yashraj.haridas22@pccoepune.org>
2026-07-14 00:13:33 +0530

    create dto, basic structure, project entity and started from bottom

---
+++ src/main/java/org/haridas/ezploy/dto/request/CreateProjectRequest.java
@@ -0,0 +1,30 @@
+package org.haridas.ezploy.dto.request;
+
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.haridas.ezploy.enums.Framework;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateProjectRequest {
+
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 2, max = 50, message = "Name must be in 2-50 characters")
+    private String name;
+
+    @NotBlank(message = "Repository URL cannot be empty")
+    private String repositoryUrl;
+
+    @Size(max = 200, message = "Description must be less than 200 characters")
+    private String description;
+
+    @NotNull
+    private Framework framework;
+}
---
+++ src/main/java/org/haridas/ezploy/enums/Framework.java
@@ -0,0 +1,12 @@
+package org.haridas.ezploy.enums;
+
+public enum Framework {
+    REACT,
+    VUE,
+    NEXT_JS,
+    NUXT,
+    EXPRESS,
+    SPRING_BOOT,
+    FAST_API,
+    STATIC_SITE
+}
