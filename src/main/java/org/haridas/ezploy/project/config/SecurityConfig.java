e91e4acb051b589281d19f0edb3aa397ef965e5e
yashraj-pradip-haridas <174370480+Yashraj-Pradip-Haridas@users.noreply.github.com>
2026-09-14 22:25:07 +0530

    Basic JWT authentication pipeline is working end to end

--- src/main/java/org/haridas/ezploy/project/config/SecurityConfig.java
+++ src/main/java/org/haridas/ezploy/project/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.haridas.ezploy.project.config;
 
+import org.haridas.ezploy.project.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -31,6 +39,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 )
                 // configuration goes here
                 .build();
